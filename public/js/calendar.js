1408d1b8788173d3e8a625147e66ff4db7345d77
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-04-04 13:05:02 -0400

    new calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,256 +1,158 @@
-//global variables
-var monthEl = $(".c-main");
-var dataCel = $(".c-cal__cel");
-var dateObj = new Date();
-var month = dateObj.getUTCMonth() + 1;
-var day = dateObj.getUTCDate();
-var year = dateObj.getUTCFullYear();
-var monthText = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-var indexMonth = month;
-var todayBtn = $(".c-today__btn");
-var addBtn = $(".js-event__add");
-var saveBtn = $(".js-event__save");
-var closeBtn = $(".js-event__close");
-var winCreator = $(".js-event__creator");
-var inputDate = $(this).data();
-today = year + "-" + month + "-" + day;
+var monthNamesRy = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var daysOfTheWeekRy = ["S", "M", "T", "W", "T", "F", "S"]
+
+var d = new Date();
+var year = d.getFullYear();// 2016
+document.querySelector("#year").innerHTML = year;
+var thisMonth = d.getMonth();// 0 - 11
+var today = d.getDate();// 1 -31
+//var nthday = d.getDay();// 0 - 7
+var daysOfTheMonthDiv = document.querySelectorAll(".daysOfTheMonth");
 
 
-// ------ set default events -------
-function defaultEvents(dataDay,dataName,dataNotes,classTag){
-  var date = $('*[data-day='+dataDay+']');
-  date.attr("data-name", dataName);
-  date.attr("data-notes", dataNotes);
-  date.addClass("event");
-  date.addClass("event--" + classTag);
+for( var month = 0; month< 12; month++){
+	createCalendar(month);
 }
 
-defaultEvents(today, 'YEAH!','Today is your day','important');
-defaultEvents('2017-12-25', 'MERRY CHRISTMAS','A lot of gift!!!!','festivity');
-defaultEvents('2017-05-04', "LUCA'S BIRTHDAY",'Another gifts...?','birthday');
-defaultEvents('2017-03-03', "MY LADY'S BIRTHDAY",'A lot of money to spent!!!!','birthday');
 
 
-// ------ functions control -------
+function createCalendar(month){
+var monthDiv = createMonthHeader(month);
 
-//button of the current day
-todayBtn.on("click", function() {
-  if (month < indexMonth) {
-    var step = indexMonth % month;
-    movePrev(step, true);
-  } else if (month > indexMonth) {
-    var step = month - indexMonth;
-    moveNext(step, true);
-  }
-});
+var firstDayOfTheMonth  = getFirstDayOfTheMonth(year,month);
+var daysinmonth  = daysInMonth(year,month)
+var counter = 0,order = 6;
 
-//higlight the cel of current day
-dataCel.each(function() {
-  if ($(this).data("day") === today) {
-    $(this).addClass("isToday");
-    fillEventSidebar($(this));
-  }
-});
-
-//window event creator
-addBtn.on("click", function() {
-  winCreator.addClass("isVisible");
-  $("body").addClass("overlay");
-  dataCel.each(function() {
-    if ($(this).hasClass("isSelected")) {
-      today = $(this).data("day");
-      document.querySelector('input[type="date"]').value = today;
-    } else {
-      document.querySelector('input[type="date"]').value = today;
-    }
-  });
-});
-closeBtn.on("click", function() {
-  winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
-});
-saveBtn.on("click", function() {
-  var inputName = $("input[name=name]").val();
-  var inputDate = $("input[name=date]").val();
-  var inputNotes = $("textarea[name=notes]").val();
-  var inputTag = $("select[name=tags]")
-    .find(":selected")
-    .text();
-
-  dataCel.each(function() {
-    if ($(this).data("day") === inputDate) {
-      if (inputName != null) {
-        $(this).attr("data-name", inputName);
-      }
-      if (inputNotes != null) {
-        $(this).attr("data-notes", inputNotes);
-      }
-      $(this).addClass("event");
-      if (inputTag != null) {
-        $(this).addClass("event--" + inputTag);
-      }
-      fillEventSidebar($(this));
-    }
-  });
-
-  winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
-  $("#addEvent")[0].reset();
-});
-
-//fill sidebar event info
-function fillEventSidebar(self) {
-  $(".c-aside__event").remove();
-  var thisName = self.attr("data-name");
-  var thisNotes = self.attr("data-notes");
-  var thisImportant = self.hasClass("event--important");
-  var thisBirthday = self.hasClass("event--birthday");
-  var thisFestivity = self.hasClass("event--festivity");
-  var thisEvent = self.hasClass("event");
-  
-  switch (true) {
-    case thisImportant:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--important'>" +
-        thisName +
-        " <span> • " +
-        thisNotes +
-        "</span></p>"
-      );
-      break;
-    case thisBirthday:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--birthday'>" +
-        thisName +
-        " <span> • " +
-        thisNotes +
-        "</span></p>"
-      );
-      break;
-    case thisFestivity:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--festivity'>" +
-        thisName +
-        " <span> • " +
-        thisNotes +
-        "</span></p>"
-      );
-      break;
-    case thisEvent:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event'>" +
-        thisName +
-        " <span> • " +
-        thisNotes +
-        "</span></p>"
-      );
-      break;
-   }
-};
-dataCel.on("click", function() {
-  var thisEl = $(this);
-  var thisDay = $(this)
-  .attr("data-day")
-  .slice(8);
-  var thisMonth = $(this)
-  .attr("data-day")
-  .slice(5, 7);
-
-  fillEventSidebar($(this));
-
-  $(".c-aside__num").text(thisDay);
-  $(".c-aside__month").text(monthText[thisMonth - 1]);
-
-  dataCel.removeClass("isSelected");
-  thisEl.addClass("isSelected");
-
-});
-
-//function for move the months
-function moveNext(fakeClick, indexNext) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "-=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "-=100%"
-    });
-    switch (true) {
-      case indexNext:
-        indexMonth += 1;
-        break;
-    }
-  }
+for( var i = 0; i < firstDayOfTheMonth +7; i++){
+order++;
+createDay(month, "&nbsp;",order, monthDiv);
 }
-function movePrev(fakeClick, indexPrev) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "+=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "+=100%"
-    });
-    switch (true) {
-      case indexPrev:
-        indexMonth -= 1;
-        break;
-    }
-  }
+for( var i = firstDayOfTheMonth; i < daysInMonth(year,month) + firstDayOfTheMonth; i++){
+counter ++;
+order++;
+createDay(month,counter,order,monthDiv);
 }
 
-//months paginator
-function buttonsPaginator(buttonId, mainClass, monthClass, next, prev) {
-  switch (true) {
-    case next:
-      $(buttonId).on("click", function() {
-        if (indexMonth >= 2) {
-          $(mainClass).css({
-            left: "+=100%"
-          });
-          $(monthClass).css({
-            left: "+=100%"
-          });
-          indexMonth -= 1;
-        }
-        return indexMonth;
-      });
-      break;
-    case prev:
-      $(buttonId).on("click", function() {
-        if (indexMonth <= 11) {
-          $(mainClass).css({
-            left: "-=100%"
-          });
-          $(monthClass).css({
-            left: "-=100%"
-          });
-          indexMonth += 1;
-        }
-        return indexMonth;
-      });
-      break;
-  }
+for( var i = firstDayOfTheMonth + daysinmonth; i < 6*7; i++){
+order++;
+createDay(month, "&nbsp;",order, monthDiv);
 }
 
-buttonsPaginator("#next", monthEl, ".c-paginator__month", false, true);
-buttonsPaginator("#prev", monthEl, ".c-paginator__month", true, false);
 
-//launch function to set the current month
-moveNext(indexMonth - 1, false);
+}
 
-//fill the sidebar with current day
-$(".c-aside__num").text(day);
-$(".c-aside__month").text(monthText[month - 1]);
+function createDay(month,counter,order,monthDiv){
+
+//if(order == 8){order = -1}
+var day = document.createElement("div");
+if(month == thisMonth && counter == today){day.setAttribute("class","to day");
+}else{
+day.setAttribute("class","day");}
+day.setAttribute("style","order:"+order);
+day.innerHTML = counter;
+monthDiv.appendChild(day);
+/*
+<div class="monthDiv">
+<div class="day">5</div>
+*/	
+}
+
+
+function createMonthHeader(month){
+var calendar = document.querySelector(".calendar");
+	
+
+	
+var monthDiv = document.createElement("div");
+	monthDiv.setAttribute("class","month");
+	calendar.appendChild(monthDiv);
+	
+var h4 = document.createElement("h4");
+	h4.innerHTML = monthNamesRy[month];
+	monthDiv.appendChild(h4);	
+	
+for( var i = 0; i < 7; i++){
+	//var order = (i == 0) ? order = 7 : order = i;
+	var hday = document.createElement("div");
+	hday.setAttribute("class","day OfWeek");
+	hday.setAttribute("style","order:"+i);
+	hday.innerHTML = daysOfTheWeekRy[i].toUpperCase();
+	monthDiv.appendChild(hday);
+}
+
+
+	return monthDiv;
+	
+/*
+<div class="month">
+	
+<div class="monthHeader">
+<div class="day OfWeek">Sun</div>
+<div class="day OfWeek">Mon</div>
+<div class="day OfWeek">Tue</div>
+<div class="day OfWeek">Wed</div>
+<div class="day OfWeek">Thu</div>
+<div class="day OfWeek">Fri</div>
+<div class="day OfWeek">Sat</div>
+</div>
+		
+<div class="daysOfTheMonth">
+*/
+}
+
+
+function daysInMonth(year,month) {
+    return new Date(year, month+1, 0).getDate();//29/03/2016 (month + 1)
+}
+
+/*function leapYear(year){
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function getNextMonth(month){
+ if (month == 11) {
+    var nextMonth = 0;
+} else {
+    var nextMonth = month+1;
+}
+return nextMonth;
+}
+*/
+function getMonthName(month){
+	return monthNamesRy[month];
+}
+function getDayName(day){
+	return daysOfTheWeekRy[day];
+}
+
+function getFirstDayOfTheMonth(y,m){
+	var firstDay = new Date(y, m, 1);
+	return firstDay.getDay();
+}
+function getLastDayOfTheMonth(y,m){
+	var lastDay = new Date(y, m + 1, 0);
+	return lastDay.getDay();
+}
+
+
+// the popp up
+
+var calendar = document.querySelector(".calendar");
+var cloneCont = document.querySelector(".cloneCont");
+var requestId = false;
+	calendar.addEventListener("click", function(e){
+        if(this.querySelector(".cloneCont")){
+			this.removeChild(this.querySelector(".cloneCont"));}
+
+        else if(e.target.parentNode.className =='month' ){
+		
+		var monthClone = e.target.parentNode.cloneNode(true);
+		monthClone.className += " cloneMonth";
+		var cloneCont = document.createElement("div");
+		cloneCont.className += " cloneCont";
+		cloneCont.appendChild(monthClone);
+		this.appendChild(cloneCont);
+		
+		}
+		},false);
+		
