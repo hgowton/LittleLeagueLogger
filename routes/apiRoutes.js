90572c134ef20bb24a313322313983ae0c6af2ff
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-04-08 14:40:11 -0400

    sad css

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,36 +2,40 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Get all examples
-  app.post("/api/user", function(req, res) {
+  app.post("/api/user", function (req, res) {
     console.log("this is req.body" + req.body.name);
     //db.user needs to equal "user" in defining sequelize var
     //if it was "User" then it would be db.User
-    db.User.findOne({ where: { name: req.body.name } }).then(function(dbUser) {
+    db.User.findOne({ where: { name: req.body.name } }).then(function (dbUser) {
       res.json(dbUser);
       // console.log("this is from apiroutes.js " + dbUser);
     });
   });
 
-  app.post("/api/newUser", function(req, res) {
+  app.post("/api/newUser", function (req, res) {
     db.User.create(req.body);
     res.end();
   });
 
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  app.get("/api/games", function(req, res) {
-    db.Game.findAll({}).then(function(data) {
+  app.get("/api/games", function (req, res) {
+    db.Game.findAll({}).then(function (data) {
+      for (i = 0; i < data.length; i++) {
+        console.log(data[i].date);
+      }
+
       res.json(data);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
